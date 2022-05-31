cf25ed8ec4f36d9e7b64006417ffde27c91599f6
MayaSelena773 <maya.bono721@gmail.com>
2022-05-31 17:00:02 +0200

    passwordend

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-document.writeln ('Hello World!');
+//Chiedo i dati all'utente
+const userName = prompt("Scrivi qui il tuo nome");
+const userLastName = prompt("Scrivi qui il tuo cognome");
+const userFavoriteColor = prompt("Scrivi qui il tuo colore preferito");
+
+//Creo la password unendo i dati
+let passWord = userName + userLastName + userFavoriteColor;
+console.log(passWord);
+
+//Stampo il risultato in pagina
+document.getElementById('result').innerHTML = passWord;
