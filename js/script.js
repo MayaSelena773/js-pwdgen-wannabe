b66d94d85f277183707767689f96fb6a09b1e493
MayaSelena773 <maya.bono721@gmail.com>
2022-06-01 09:26:04 +0200

    number

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 const userName = prompt("Scrivi qui il tuo nome");
 const userLastName = prompt("Scrivi qui il tuo cognome");
 const userFavoriteColor = prompt("Scrivi qui il tuo colore preferito");
+const myNumber = 21;
 
 //Creo la password unendo i dati
-let passWord = userName + userLastName + userFavoriteColor;
+let passWord = userName + userLastName + userFavoriteColor + myNumber;
 console.log(passWord);
 
 //Stampo il risultato in pagina
